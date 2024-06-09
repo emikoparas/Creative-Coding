af78fdc5f914ecd9c7c58fe7020d8bc019f897f5
emikoparas <144320280+emikoparas@users.noreply.github.com>
2024-06-09 18:47:30 +0400

    TYPOGRAPHY

---
+++ ACT 5/sketch.js
@@ -0,0 +1,12 @@
+
+function setup(){
+  createCanvas(400,400);
+}
+function draw(){
+  background(220);
+  fill(0,0,0)
+  textFont('Monoton');
+  //'monoton' is the font name in the link.
+  text('The Art of Noticing',125,200);
+  textSize(24)
+}
